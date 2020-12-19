59cb2c2dfee7b6917e4b3a670a532205bb7c61cc
Pirgo <pierogmateusz@wp.pl>
2020-12-19 18:10:39 +0100

    fixed sorting

--- src/ObjectsOnMap/AnimalSorter.java
+++ src/ObjectsOnMap/AnimalSorter.java
@@ -6,10 +6,10 @@ public class AnimalSorter implements Comparator<Animal> {
     @Override
     public int compare(Animal o1, Animal o2) {
         if(o1.getEnergy() - o2.getEnergy() < 0){
-            return 1;
+            return -1;
         }
         if(o1.getEnergy() - o2.getEnergy() > 0){
-            return -1;
+            return 1;
         }
         return 0;
     }
